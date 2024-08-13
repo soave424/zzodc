931188d3b6d93b05a991ddf356f6a2a4d64bae15
soave424 <89090628+soave424@users.noreply.github.com>
2024-08-13 14:59:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-st.title('첫웹앱만들기🤖')
+
+st.title('첫 웹앱 만들기🤖')
 name = st.text_input('이름을 입력해주세요!👨🏻‍💻')
 menu = st.selectbox('좋아하는 음식을 선택해주세요!', ['떡볶이', '순대', '어묵', '김말이'])
-if st.button('인사말 생성하기') : 
-  st.write(name + '님! 당신이 좋아하는 음식은 '+ menu +'이군요! 저도 좋아해요!)
-           
+if st.button('인사말 생성하기'):
+    st.write(f'{name}님! 당신이 좋아하는 음식은 {menu}이군요! 저도 좋아해요!')
